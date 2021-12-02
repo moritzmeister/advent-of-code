0b9c4c3d8f73f132835687aca10385e579c8492f
moritzmeister <meister.mo@gmail.com>
2021-12-02 23:07:36 +0100

    day 1 part 2

--- 2021/day-1/main.go
+++ 2021/day-1/main.go
@@ -9,7 +9,58 @@ import (
 )
 
 func main() {
-	file, err := os.Open("../inputs/day-1.txt")
+	var path string = "../inputs/day-1.txt"
+	//var tpath string = "../inputs/day-1-test.txt"
+	fmt.Println("Final part1 increase count: ", part1(path))
+	fmt.Println("Final part2 increase count: ", part2(path))
+}
+
+func sum(array [3]int) int {
+	result := 0
+	for _, v := range array {
+		result += v
+	}
+	return result
+}
+
+func part2(path string) int {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	i := 0
+	count := 0
+	increase_count := 0
+	var current, prev_sum, current_sum int
+	var window [3]int
+
+	for scanner.Scan() {
+		current, _ = strconv.Atoi(scanner.Text())
+		window[i] = current
+		if i == 2 {
+			i = 0
+		} else {
+			i++
+		}
+		current_sum = sum(window)
+		if count > 2 {
+			if current_sum > prev_sum {
+				increase_count++
+			}
+		}
+		count++
+		//fmt.Println(current, window, prev_sum, current_sum, increase_count)
+		prev_sum = current_sum
+	}
+	return increase_count
+}
+
+func part1(path string) int {
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,23 +75,19 @@ func main() {
 	for scanner.Scan() {
 		current, _ = strconv.Atoi(scanner.Text())
 		if i == 1 {
-			prev = current
 			i++
 		} else {
 			if current > prev {
 				// fmt.Println("Prev, current: ", prev, ", ", current)
 				increase_count++
-				prev = current
-			} else {
-				prev = current
 			}
 		}
-		fmt.Println(current)
+		prev = current
+		// fmt.Println(current)
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Final increase count: ", increase_count)
+	return increase_count
 }
